c6c1aa2983f9d95df4b1fb3152927ac7ed48ad96
Eechon <545429204@qq.com>
2019-07-30 10:07:42 +0800

    0730express创建服务器

--- 0730/express.js
+++ 0730/express.js
@@ -1,2 +1,12 @@
+const express = require("express");
 const app = express();
- 
+app.listen(8080, () => {
+  console.log("服务器已经启动，express");
+});
+
+app.use("/views", express.stratic("views"));
+app.use("/assets"), express.static("assets ");
+
+app.get("/", (req, res) => {
+  res.send("hello,world");
+});
